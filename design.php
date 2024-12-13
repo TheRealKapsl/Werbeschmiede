19ddcd470a63aeda62b811933f4cea51b720f1be
TheRealKapsl <office@werbeschmiede.eu>
2024-12-13 07:23:47 +0100

    Update design.php
    text changes

--- design.php
+++ design.php
@@ -92,39 +92,9 @@
       <div class="row">
         <div class="col-lg-12">
           <div class="mb-6">
-            <h2 class="h5 text-uppercase">Article 1 - Nulla pharetra nisl</h2>
+            <h2 class="h5 text-uppercase">Work in Progress</h2>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus pellentesque diam. Vestibulum ultrices tellus ac ullamcorper tempor. Vivamus nisl mi, tempus non quam sed, mattis porttitor purus. Sed posuere leo id vehicula consequat. Duis nunc risus, vulputate sit amet enim et, iaculis facilisis sapien. Phasellus sagittis vestibulum odio, at pellentesque diam laoreet id. Praesent imperdiet iaculis risus nec vestibulum.</p>
-            
-            <p>Praesent nec ligula laoreet, dictum ligula ornare, consectetur erat. Sed consectetur euismod euismod. Aliquam maximus arcu et neque luctus, sodales convallis odio rutrum. Vestibulum gravida ut sapien non fermentum. Sed euismod fringilla enim. Vestibulum congue libero at justo sodales cursus vitae ac nisi. Ut ligula turpis, ultrices maximus bibendum at, consectetur non magna. Proin pretium eleifend purus, sit amet rutrum nunc fringilla in.</p>
-            
-            <p class="mb-2">Morbi faucibus laoreet lectus quis pellentesque :</p>
-
-            <ul class="list-group">
-              <li class="list-group">- Vivamus in arcu in sem luctus auctor ut at neque. Morbi sed tortor dignissim, gravida ligula fringilla, sollicitudin tortor.</li>
-              <li class="list-group">- Phasellus ipsum nibh, vehicula non risus a, tempus ultrices dolor. Duis sed lacinia tellus, eget dignissim mi.</li>
-              <li class="list-group">- Donec rhoncus pulvinar urna, vitae sodales diam accumsan vitae. Sed nulla risus, semper nec mauris ac, bibendum ultricies magna.</li>
-              <li class="list-group">- Nullam id mi neque. Etiam ullamcorper mauris augue, vitae tincidunt ipsum placerat eget.</li>
-              <li class="list-group">- Vivamus ac dui leo. Phasellus sit amet diam faucibus, accumsan diam non, porttitor metus. Duis aliquam posuere nisl id elementum.</li>
-              <li class="list-group">- Donec a bibendum est. Aliquam sed massa non nibh lacinia egestas.</li>
-            </ul>
-          </div>
-
-          <div>
-            <h2 class="h5 text-uppercase">Article 2 - Dignissim euismod</h2>
-            
-            <p>In molestie ipsum id leo faucibus molestie vitae eget sem. Suspendisse luctus lacus dolor, sed vehicula lorem rhoncus sed. In dapibus feugiat dui, sit amet laoreet enim. Aliquam enim nisl, suscipit a pellentesque eget, condimentum quis nisi. Pellentesque faucibus leo sed quam euismod, sit amet condimentum sem interdum. Quisque leo mi, vestibulum dignissim dolor et, porttitor rutrum neque. Vivamus cursus arcu in ex congue porta. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc at sem tortor. Etiam a blandit leo, vel elementum nunc. Proin sagittis porttitor pretium.</p>
-            
-            <p class="mb-2">Proin nec sollicitudin nulla vestibulum id lobortis ex, aliquet dignissim justo :</p>
-
-            <ul class="list-group mb-4">
-              <li class="list-group">1. Nulla porta bibendum lacus nec ultricies.</li>
-              <li class="list-group">2. Donec feugiat tincidunt neque, ac semper risus accumsan ut.</li>
-              <li class="list-group">3. Aliquam auctor tellus lacus, at volutpat velit semper eget.</li>
-              <li class="list-group">4. Etiam cursus eros dui, eu lacinia est venenatis et. Integer lorem ante.</li>
-            </ul>
-
-            <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris at odio mauris. Aliquam luctus mattis euismod. Aliquam eget molestie ipsum. Donec rutrum arcu metus, id sagittis ante tempus sed. Donec eu risus eros. Suspendisse consectetur metus orci, ut gravida elit ultricies in. Etiam tristique porta felis, in mollis est feugiat ornare. Fusce porta molestie leo ut venenatis. Integer semper lorem eu mattis posuere.</p>
+            <p>Wir abreiten aktuell an einem ausführlichen Text!</p>
           </div>
         </div>
       </div>
